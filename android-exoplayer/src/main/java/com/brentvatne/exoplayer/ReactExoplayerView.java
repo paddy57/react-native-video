3a85aeb7523cfdaa0f538dfcfce475507788268c
Yogendra <ddevstack@gmail.com>
2020-06-02 17:59:16 +0530

    When I mute a video then swipe a few times the sound comes back on itself

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -393,11 +393,17 @@ class ReactExoplayerView extends FrameLayout implements
                     eventEmitter.loadStart();
                     loadVideoStarted = true;
                 }
-
+    
                 // Initializing the playerControlView
                 initializePlayerControl();
                 setControls(controls);
                 applyModifiers();
+
+                if(ReactExoplayerViewManager.getMuted()){
+                    player.setVolume(0.f);
+                }else{
+                    player.setVolume(1.f);
+                }
             }
         }, 1);
     }
